0e70541394a75fe1ca97aad90e6d6c919a25a02c
aashied12 <49364096+aashied12@users.noreply.github.com>
2023-04-01 16:23:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ return data.meals;
 
 // Get meal by ID
 async function getMealByID(id) {
-const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${id}`);
 const data = await response.json();
 return data.meals || [];
 }
