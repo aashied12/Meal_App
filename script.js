3fcffde95cbb36784941f637ee18d199789738c2
aashied12 <49364096+aashied12@users.noreply.github.com>
2023-04-01 16:33:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -54,6 +54,7 @@ updateFavouritesUI();
 
 // Get meals by search query
 async function getMealsBySearch(query) {
+  console.log(query);
 const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
 const data = await response.json();
 return data.meals;
