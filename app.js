969de5bf70d4959c0bd11f27f1dd64bbac93888d
aashied12 <49364096+aashied12@users.noreply.github.com>
2023-04-01 13:21:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,125 @@
-// Array to store meals
-let meals = [];
+// Array to store favourite meals
+let favourites = [];
 
-// Get input and button elements
-const mealInput = document.getElementById('mealInput');
-const addMealBtn = document.getElementById('addMealBtn');
+// Get search input and button elements
+const searchInput = document.getElementById('searchInput');
+const searchBtn = document.getElementById('searchBtn');
 
-// Get ul element to display meals
-const mealList = document.getElementById('mealList');
+// Get search results ul element
+const searchResults = document.getElementById('searchResults');
 
-// Function to add meal to array and display on the page
-function addMeal() {
-  // Get input value and clear input field
-  const mealName = mealInput.value.trim();
-  mealInput.value = '';
+// Function to search for meals and update the display
+async function searchMeals() {
+  // Get search query and clear input field
+  const query = searchInput.value.trim().toLowerCase();
+  searchInput.value = '';
 
-  // If meal name is not empty, add it to the array and update the display
-  if (mealName) {
-    meals.push(mealName);
-    updateDisplay();
+  // Make API call to search for meals
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+  const data = await response.json();
+
+  // If no results, show message
+  if (!data.meals || data.meals.length === 0) {
+    searchResults.innerHTML = '<p>No results found.</p>';
+    return;
+  }
+
+  // Clear current search results
+  searchResults.innerHTML = '';
+
+  // Loop through each meal result and add to the display
+  data.meals.forEach(meal => {
+    const mealItem = document.createElement('li');
+    mealItem.className = 'list-group-item d-flex justify-content-between align-items-center';
+    mealItem.innerHTML = `
+      <span>${meal.strMeal}</span>
+      <button class="btn btn-primary" data-mealid="${meal.idMeal}" data-toggle="modal" data-target="#mealModal">Details</button>
+      <button class="btn btn-secondary" data-mealid="${meal.idMeal}" onclick="addToFavourites(${meal.idMeal})">Add to Favourites</button>
+    `;
+    searchResults.appendChild(mealItem);
+  });
+}
+
+// Function to add meal to favourites array and display success message
+function addToFavourites(mealId) {
+  const meal = favourites.find(m => m.idMeal === mealId);
+  if (!meal) {
+    const newMeal = data.meals.find(m => m.idMeal === mealId);
+    favourites.push(newMeal);
+    localStorage.setItem('favourites', JSON.stringify(favourites));
+    alert(`${newMeal.strMeal} has been added to your favourites.`);
+  } else {
+    alert(`${meal.strMeal} is already in your favourites.`);
   }
 }
 
-// Function to update the display with the current array of meals
-function updateDisplay() {
-  // Clear the current display
-  mealList.innerHTML = '';
+// Function to display favourite meals
+function showFavourites() {
+  // Get favourites from local storage
+  favourites = JSON.parse(localStorage.getItem('favourites')) || [];
 
-  // If there are no meals, show a message
-  if (meals.length === 0) {
-    const noMealsMsg = document.createElement('p');
-    noMealsMsg.textContent = 'No meals added yet.';
-    mealList.appendChild(noMealsMsg);
-  } else {
-    // Create a list item for each meal and add it to the ul element
-    for (let i = 0; i < meals.length; i++) {
-      const mealItem = document.createElement('li');
-      mealItem.className = 'list-group-item';
-      mealItem.textContent = meals[i];
-      mealList.appendChild(mealItem);
+  // If no favourites, show message
+  if (favourites.length === 0) {
+    searchResults.innerHTML = '<p>No favourites added yet.</p>';
+    return;
+  }
+
+  // Clear current search results
+  searchResults.innerHTML = '';
+
+  // Loop through each favourite meal and add to the display
+  favourites.forEach(meal => {
+    const mealItem = document.createElement('li');
+    mealItem.className = 'list-group-item d-flex justify-content-between align-items-center';
+    mealItem.innerHTML = `
+      <span>${meal.strMeal}</span>
+      <button class="btn btn-primary" data-mealid="${meal.idMeal}" data-toggle="modal" data-target="#mealModal">Details</button>
+    `;
+    searchResults.appendChild(mealItem);
+  });
+}
+
+// Event listener for search button
+searchBtn.addEventListener('click', searchMeals);
+
+// Event listener for enter key press in search input
+searchInput.addEventListener('keyup', e => {
+  if (e.keyCode === 13) {
+    searchMeals();
+  }
+});
+
+// Modal event listener to display meal details
+$('#mealModal').on('show.bs.modal', async function (event) {
+  const button = $(event.relatedTarget);
+ 
+  const mealId = button.data('mealid');
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`);
+  const data = await response.json();
+  const meal = data.meals[0];
+
+  const modal = $(this);
+  modal.find('.modal-title').text(meal.strMeal);
+  modal.find('.modal-body').html(`
+    <img src="${meal.strMealThumb}" alt="${meal.strMeal}" class="img-fluid mb-2">
+    <h5>Ingredients:</h5>
+    <ul>
+      ${getIngredients(meal).map(ingredient => `<li>${ingredient}</li>`).join('')}
+    </ul>
+    <h5>Instructions:</h5>
+    <p>${meal.strInstructions}</p>
+  `);
+});
+
+// Function to get ingredients array from meal object
+function getIngredients(meal) {
+  const ingredients = [];
+  for (let i = 1; i <= 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+    } else {
+      break;
     }
   }
+  return ingredients;
 }
-
-// Event listener for add meal button
-addMealBtn.addEventListener('click', addMeal);
