03bf70076f3576c27dd4295cca1f9833b81bc2e8
aashied12 <49364096+aashied12@users.noreply.github.com>
2023-04-01 16:15:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,91 +8,105 @@ const favouriteMealsContainer = document.querySelector('#favourite-meals');
 
 // Event listener for search form submit
 searchForm.addEventListener('submit', async (event) => {
-  event.preventDefault();
+event.preventDefault();
 
-  // Clear previous search results
-  mealsContainer.innerHTML = '';
+// Clear previous search results
+mealsContainer.innerHTML = '';
 
-  // Get search query
-  const query = searchInput.value.trim();
+// Get search query
+const query = searchInput.value.trim();
 
   // Check if query is not empty
-  if (query !== '') {
-    // Fetch meals from API
-    const meals = await getMealsBySearch(query);
+if (query !== '') {
+// Fetch meals from API
+const meals = await getMealsBySearch(query);
 
-    // Display meals
-    displayMeals(meals);
-  }
+// Display meals
+displayMeals(meals);
+
+}
 });
+
 
 // Event listener for meal container click
 mealsContainer.addEventListener('click', async (event) => {
-  const mealInfo = event.target.closest('.meal').querySelector('.meal-info');
-  if (mealInfo) {
-    const mealID = mealInfo.getAttribute('data-mealid');
-    const meal = await getMealByID(mealID);
-    displayMealDetail(meal);
-  }
+const mealInfo = event.target.closest('.meal').querySelector('.meal-info');
+if (mealInfo) {
+const mealID = mealInfo.getAttribute('data-mealid');
+const meal = await getMealByID(mealID);
+displayMealDetail(meal);
+}
 });
+
 // Event listener for favourite button click
-mealsContainer.addEventListener('click', async (event) => {
-  if (event.target.classList.contains('btn-favourite')) {
-    const mealElement = event.target.closest('.meal');
-    const mealID = mealElement.getAttribute('data-mealid');
+favouriteMealsContainer.addEventListener('click', async (event) => {
+if (event.target.classList.contains('btn-favourite')) {
+const mealElement = event.target.closest('.meal');
+const mealID = mealElement.getAttribute('data-mealid');
 
-    // Add meal to favourites
-    addMealToFavourites(mealID);
+addMealToFavourites(mealID);
 
-    // Update the UI
-    updateFavouritesUI();
-  }
+// Update the UI
+updateFavouritesUI();
+
+}
 });
 
 // Get meals by search query
 async function getMealsBySearch(query) {
-  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
-  const data = await response.json();
-  return data.meals;
+const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=${query}');
+const data = await response.json();
+return data.meals;
 }
 
 // Get meal by ID
 async function getMealByID(id) {
-  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
-  const data = await response.json();
-  return data.meals || [];
+const response = await fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}');
+const data = await response.json();
+return data.meals || [];
 }
 
 
 // Display meals
-function displayMealDetail(mealId) {
-  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
-    .then(response => response.json())
-    .then(data => {
-      const meal = data.meals[0];
-      if (!meal) {
-        throw new Error(`No meal found with ID ${mealId}`);
-      }
-      const mealDetailContainer = document.getElementById("meal-detail-container");
+// Display meals
+function displayMeals(meals) {
+  // Check if any meals were found
+  if (meals === null || meals.length === 0) {
+    resultHeading.innerHTML = `<p>No meals found for search query.</p>`;
+    return;
+  }
 
-      // Create HTML elements for the meal detail
-      const mealDetail = document.createElement("div");
-      mealDetail.classList.add("meal-detail");
-      mealDetail.innerHTML = `
-        <h2>${meal.strMeal}</h2>
+  // Clear previous search results
+  mealsContainer.innerHTML = '';
+
+  // Display search results heading
+  resultHeading.innerHTML = `<h2>Search results for '${searchInput.value}':</h2>`;
+
+  // Display meals
+  meals.forEach((meal) => {
+    // Create HTML elements for the meal
+    const mealElement = document.createElement('div');
+    mealElement.classList.add('meal');
+    mealElement.innerHTML = `
+      <div class="meal-img">
         <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
-        <p>${meal.strInstructions}</p>
-        <button class="btn-favourite" data-mealid="${meal.idMeal}">Add to favourites</button>
-      `;
+      </div>
+      <div class="meal-info" data-mealid="${meal.idMeal}">
+        <h3>${meal.strMeal}</h3>
+        <button class="btn-favourite" data-mealid="${meal.idMeal}">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#fff" width="18px" height="18px">
+            <path d="M12 21.35L5.16 17.1C2.48 15.48 1 13.06 1 10.5 1 6.36 4.36 3 8.5 3c2.02 0 3.89.82 5.5 2.15C16.61 3.82 18.48 3 20.5 3 24.64 3 28 6.36 28 10.5c0 2.56-1.48 5-3.16 6.6L12 21.35z"/>
+          </svg>
+        </button>
+      </div>
+    `;
 
-      // Add the meal detail to the container
-      mealDetailContainer.innerHTML = "";
-      mealDetailContainer.appendChild(mealDetail);
-    })
-    .catch(error => {
-      console.error(`Error fetching meal detail: ${error}`);
-    });
+    // Add the meal to the container
+    mealsContainer.appendChild(mealElement);
+  });
 }
+
+
 
 
 // Display meal detail
