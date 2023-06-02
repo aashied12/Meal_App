6aad00716e360b1593be36efbd26ccf8e4ae1cf6
aashied12 <49364096+aashied12@users.noreply.github.com>
2023-06-02 13:55:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const searchForm = document.getElementById('searchForm');
   const searchInput = document.getElementById('searchInput');
   const mealResults = document.getElementById('mealResults');
+  const favoritesList = document.getElementById('favoritesList');
 
   searchInput.addEventListener('input', handleSearchInput);
+  mealResults.addEventListener('click', handleAddToFavorites);
 
   async function handleSearchInput(event) {
     const searchTerm = event.target.value;
@@ -46,10 +48,20 @@ document.addEventListener('DOMContentLoaded', () => {
     cardBody.classList.add('card-body');
     card.appendChild(cardBody);
 
+    const titleContainer = document.createElement('div');
+    titleContainer.classList.add('d-flex', 'justify-content-between', 'align-items-center');
+    cardBody.appendChild(titleContainer);
+
     const title = document.createElement('h5');
     title.classList.add('card-title');
     title.textContent = meal.strMeal;
-    cardBody.appendChild(title);
+    titleContainer.appendChild(title);
+
+    const heartButton = document.createElement('button');
+    heartButton.classList.add('btn', 'btn-outline-danger', 'btn-sm');
+    heartButton.innerHTML = '<i class="bi bi-heart"></i>';
+    heartButton.dataset.mealId = meal.idMeal;
+    titleContainer.appendChild(heartButton);
 
     const description = document.createElement('p');
     description.classList.add('card-text');
@@ -57,5 +69,27 @@ document.addEventListener('DOMContentLoaded', () => {
     cardBody.appendChild(description);
 
     return card;
+  }
+
+  function handleAddToFavorites(event) {
+    if (event.target.tagName === 'BUTTON') {
+      const mealId = event.target.dataset.mealId;
+      const mealCard = event.target.closest('.card');
+      const mealTitle = mealCard.querySelector('.card-title').textContent;
+      const favoriteItem = createFavoriteItem(mealId, mealTitle);
+      favoritesList.appendChild(favoriteItem);
+    }
+  }
+
+  function createFavoriteItem(mealId, mealTitle) {
+    const listItem = document.createElement('li');
+    listItem.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
+    listItem.innerHTML = `
+      <span>${mealTitle}</span>
+      <button class="btn btn-outline-danger btn-sm" data-meal-id="${mealId}">
+        <i class="bi bi-trash"></i>
+      </button>
+    `;
+    return listItem;
   }
 });
