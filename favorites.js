95bc6679f2229f89c61ba0339e31c8f7105d5ed5
aashied12 <49364096+aashied12@users.noreply.github.com>
2023-06-03 13:23:39 +0530

    Update favorites.js

--- favorites.js
+++ favorites.js
@@ -13,22 +13,33 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  function createFavoriteItem(mealId, mealTitle) {
-    const listItem = document.createElement('li');
-    listItem.classList.add('list-group-item');
-    listItem.textContent = mealTitle;
+  function createFavoriteItem(mealId, mealTitle, mealImage) {
+  const listItem = document.createElement('li');
+  listItem.classList.add('list-group-item');
 
-    const removeButton = document.createElement('button');
-    removeButton.classList.add('btn', 'btn-outline-danger', 'btn-sm', 'float-end');
-    removeButton.innerHTML = '<i class="bi bi-trash"></i>';
-    removeButton.addEventListener('click', () => {
-      removeFavorite(mealId);
-      listItem.remove();
-    });
+  const mealImageElement = document.createElement('img');
+  mealImageElement.src = mealImage;
+  mealImageElement.alt = mealTitle;
+  mealImageElement.classList.add('meal-image');
+  listItem.appendChild(mealImageElement);
 
-    listItem.appendChild(removeButton);
-    return listItem;
-  }
+  const mealTitleElement = document.createElement('span');
+  mealTitleElement.classList.add('meal-title');
+  mealTitleElement.textContent = mealTitle;
+  listItem.appendChild(mealTitleElement);
+
+  const removeButton = document.createElement('button');
+  removeButton.classList.add('btn', 'btn-outline-danger', 'btn-sm', 'float-end');
+  removeButton.innerHTML = '<i class="bi bi-trash"></i>';
+  removeButton.addEventListener('click', () => {
+    removeFavorite(mealId);
+    listItem.remove();
+  });
+  listItem.appendChild(removeButton);
+
+  return listItem;
+}
+
 
   function removeFavorite(mealId) {
     localStorage.removeItem(mealId);
