9aeccbf3fe2f54d6ed2252d72201640b0fe4c75e
aashied12 <49364096+aashied12@users.noreply.github.com>
2023-06-03 22:35:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -122,28 +122,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 }
 
-  // Function to retrieve the favorites from local storage
-  function getFavorites() {
-    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-    return favorites;
-  }
-
-  // Function to display the favorites on the favorites page
-  function displayFavorites() {
-    const favoritesList = document.getElementById('favoritesList');
-    const favorites = getFavorites();
-
-    if (favorites.length === 0) {
-      favoritesList.innerHTML = '<p>No favorite meals found.</p>';
-      return;
-    }
-
-    favoritesList.innerHTML = '';
-    favorites.forEach((meal) => {
-      const favoriteItem = createFavoriteItem(meal);
-      favoritesList.appendChild(favoriteItem);
-    });
-  }
+  
 
   // Function to create a favorite item element
   function createFavoriteItem(meal) {
